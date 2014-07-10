799702eb68c160d4f7f7d681c8569f7ac6e0211f
Richard Shade <rshade@rightscale.com>
2014-07-10 15:40:14 -0500

    adding recipe

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,6 @@ license          'Apache 2.0'
 description      'Installs/Configures rsc_skeleton_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+
+recipe "unixbench::default", "installs unixbench"
